803614d531a696941b49c152752abe398dccd237
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-09-11 08:26:19 +0200

    added git config

--- modules/macos/cli/git/default.nix
+++ modules/macos/cli/git/default.nix
@@ -6,6 +6,12 @@
     userEmail = "kasper@kaspertherkelsen.com";
     extraConfig = {
       push.autoSetupRemote = true;
+      gpg.format = "ssh";
+      commit.gpgsign = true;
+      rerere.enabled = true;
+      delta.line-numbers = true;
+      delta.side-by-side = true;
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBmzrRZGq9PPl3KtSTuvmfH+XvJ9IRX8IeksZiVYW61";
     };
   };
 }
