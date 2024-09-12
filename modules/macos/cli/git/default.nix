fb7e296eb336ef90c1f4847c3612b9c709455ccc
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-09-12 07:55:58 +0200

    added git

--- modules/macos/cli/git/default.nix
+++ modules/macos/cli/git/default.nix
@@ -12,7 +12,7 @@
       delta.line-numbers = true;
       delta.side-by-side = true;
       user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBmzrRZGq9PPl3KtSTuvmfH+XvJ9IRX8IeksZiVYW61";
-      gpg.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
     };
   };
 }
