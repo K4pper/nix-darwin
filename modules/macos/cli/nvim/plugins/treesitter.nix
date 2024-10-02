169304879848fa4ffd3cbca4fdf18a02c167e0b8
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-10-02 16:25:54 +0200

    updated + treesitter config

--- modules/macos/cli/nvim/plugins/treesitter.nix
+++ modules/macos/cli/nvim/plugins/treesitter.nix
@@ -4,6 +4,27 @@
       enable = true;
       nixvimInjections = true;
       folding = true;
+      settings = {
+          ensure_installed = [
+          "bash"
+          "c"
+          "cmake"
+          "cpp"
+          "css"
+          "dockerfile"
+          "html"
+          "json"
+          "lua"
+          "regex"
+          "rust"
+          "toml"
+          "yaml"
+          "bicep"
+          ];
+          highlight = {
+            enable = true;
+          };
+      };
     };
   };
 }
