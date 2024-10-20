5a11790edec18eb2861d2954e1b3facb9df43542
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2024-10-20 19:23:41 +0200

    added rose pine config

--- modules/macos/cli/starship/default.nix
+++ modules/macos/cli/starship/default.nix
@@ -1,49 +1,11 @@
-{ lib, ... }:
+{lib, ...}:
+let
+  config = builtins.readFile ./starship.toml;
+in
 {
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
-    settings = {
-      format = lib.concatStrings [
-        "$username"
-        "$hostname"
-        "$directory"
-        "$git_branch"
-        "$git_state"
-        "$git_status"
-        "$kubernetes"
-        "$line_break"
-        "$character"
-      ];
-      character = {
-        success_symbol = "[❯](purple)";
-        error_symbol = "[❯](red)";
-        vimcmd_symbol = "[❮](green)";
-      };
-      git_branch = {
-        format = "[$branch]($style)";
-        style = "bright-black";
-      };
-      git_status = {
-        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
-        style = "cyan";
-        conflicted = "";
-        untracked = "";
-        modified = "";
-        staged = "";
-        renamed = "";
-        deleted = "";
-        stashed = "≡";
-      };
-      git_state = {
-        format = "'\([$state( $progress_current/$progress_total)]($style)\) '";
-        style = "bright-black";
-      };
-      kubernetes = {
-        format = "[$symbol(\($namespace\))]($style)";
-        disabled = false;
-        style = "blue";
-      };
-    };
+    settings = builtins.fromTOML config;
   };
 }
--- modules/macos/cli/tmux/default.nix
+++ modules/macos/cli/tmux/default.nix
@@ -1,7 +1,59 @@
-{...}:
+{ pkgs, ... }:
 {
   programs.tmux = {
     enable = true;
-    extraConfig = builtins.readFile ./tmux.conf;
+    clock24 = true;
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      yank
+      {
+    plugin = rose-pine;
+      extraConfig = ''
+        set -g @rose_pine_variant 'main'
+      '';
+      }
+    ];
+
+    extraConfig = ''
+      # Fix color in nvim for alacritty
+      sdefaultet -g default-terminal "tmux-256color"
+      set -ag terminal-overrides ",xterm-256color:RGB"
+
+      set -g allow-passthrough on
+
+      set -ga update-environment TERM
+      set -ga update-environment TERM_PROGRAM
+
+      set -g escape-time 10
+
+      # remap prefix from 'C-b' to 'C-a'
+      unbind C-b
+      set-option -g prefix C-a
+      bind-key C-a send-prefix
+
+      # Window bindings
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+
+      bind -n M-h select-pane -L
+      bind -n M-j select-pane -D
+      bind -n M-k select-pane -U
+      bind -n M-l select-pane -R
+
+      unbind '"'
+      unbind %
+
+      # Easier copy-mode
+      setw -g  mode-keys vi
+      bind Enter copy-mode
+      bind -n M-Enter copy-mode
+      bind -T  copy-mode-vi v send -X begin-selection
+      bind -T  copy-mode-vi y send -X copy-pipe-and-cancel
+      bind P paste-buffer
+      bind -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel
+
+      # Enable mouse
+      set -g mouse on
+      '';
   };
 }
