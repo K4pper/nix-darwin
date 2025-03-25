0fc27ed83fcbe232577e7b3b81ba21e4c7be89b0
Kasper Therkelsen <kasper@kaspertherkelsen.com>
2025-03-25 11:06:46 +0100

    az autocomplete

--- modules/macos/cli/zsh/default.nix
+++ modules/macos/cli/zsh/default.nix
@@ -14,7 +14,11 @@
     enableCompletion = true;
     dotDir = ".config/zsh";
     completionInit = "autoload -Uz compinit && zstyle ':completion:*' menu select && zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}' && zmodload zsh/complist && compinit";
-    initExtra = "unsetopt BEEP";
+    initExtra = ''
+      unsetopt BEEP
+      autoload bashcompinit && bashcompinit
+      source $(brew --prefix)/etc/bash_completion.d/az
+    '';
     profileExtra = "eval $(/opt/homebrew/bin/brew shellenv)";
   };
 }
